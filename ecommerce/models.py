7863b5a71825fe54d299d9c4e17c8da2daa089f0
linuxtusX5 <salvatusjoffrey@gmail.com>
2026-09-04 11:05:39 +0800

    password Reset Token

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -79,3 +79,29 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+
+# =========================================================
+# Password Reset Token
+# =========================================================
+
+class PasswordResetToken(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    email = models.EmailField(
+        unique=True,
+        db_index=True
+    )
+
+    token = models.CharField(
+        max_length=255,
+        unique=True
+    )
+
+    expires = models.DateTimeField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.email

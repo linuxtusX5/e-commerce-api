889795c979b6f5e27b5a03056cfe5ac385637e65
linuxtusX5 <salvatusjoffrey@gmail.com>
2026-09-18 10:12:00 +0800

    fixed order Item

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -308,7 +308,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(
         Order,
         on_delete=models.CASCADE,
-        related_name="items"
+        related_name="order_items"
     )
 
     product = models.ForeignKey(

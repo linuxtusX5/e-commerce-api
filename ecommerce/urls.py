a48c2df83befa499e7114bae70ac946b35ff1d9e
linuxtusX5 <salvatusjoffrey@gmail.com>
2026-09-21 10:01:13 +0800

    wishlist

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,6 +7,7 @@ router.register(r'categories', views.CategoryViewSet)
 router.register(r'products', views.ProductViewSet)
 router.register(r'variants', views.ProductVariantViewSet, basename='variant')
 router.register(r'orders', views.OrderViewSet, basename='order')
+router.register(r'wishlist', views.WishlistItemViewSet, basename='wishlist')
 
 urlpatterns = [
     path('auth/register/', views.register_user, name='register'),
